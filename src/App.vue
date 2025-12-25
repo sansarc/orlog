b62259259642b2237131b221cb7b3925f7390c84
sansarc <sansevieroarcangelo@gmail.com>
2025-12-25 03:14:39 +0100

    feat(app): implement main game board layout and entry point

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Toast from "primevue/toast"
+import GameBoard from "./views/GameBoard.vue";
+</script>
+
+<template>
+  <Toast position="bottom-right" class="toast-center"/>
+  <GameBoard/>
+</template>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.toast-center {
+  padding: 1.5rem;
+  transform: translateX(8%);
+}
+</style>
